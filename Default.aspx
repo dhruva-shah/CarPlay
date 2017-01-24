6a7311d404c04f05f2cd3deb568ad3f8bd3f4dac
dhruva-shah <shahdhruva811@gmail.com>
2017-01-24 12:20:30 -0500

    initial commit

---
+++ Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        Year :&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox_year" runat="server" Height="16px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        <br />
+        Make :&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox_make" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Speed :&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox_speed" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label_info" runat="server" Text="Label" Visible="False"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="Button_done" runat="server" OnClick="Button_done_Click" Text="Done" />
+        <br />
+        <asp:Panel ID="Panel1" runat="server" Visible="False">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_acc" runat="server" Text="Accelerate" OnClick="Button_acc_Click" />
+            &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_brake" runat="server" OnClick="Button_brake_Click" Text="Brake" />
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button_restart" runat="server" OnClick="Button_restart_Click" Text="Restart" />
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string car_make="";
+    public static int car_year, car_speed;
+    public static Car car;
+    public class Car
+    {
+        int speed = 0, year;
+        string make;
+        public Car(string make, int year, int speed)
+        {
+            this.make = make;
+            this.year = year;
+            this.speed = speed;
+        }
+
+        public int acc(int s)
+        {            
+            s = s + 5;
+            return s;
+        }
+        public int brake(int s)
+        {           
+            if (s <= 4)
+            {
+                return 0;
+            }
+            else
+            {
+                s = s - 5;
+                return s;
+            }
+        }
+    }
+
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+          
+    }
+
+    protected void Button_done_Click(object sender, EventArgs e)
+    {
+        
+        if ((TextBox_year.Text=="")||(TextBox_make.Text=="")||(TextBox_speed.Text==""))
+        {
+            Label_info.Text = "Please enter all values";
+            Label_info.Visible = true;
+        }
+        else
+        {
+           
+            car_make = TextBox_make.Text;
+            car_year = Convert.ToInt32(TextBox_year.Text);
+            car_speed = Convert.ToInt32(TextBox_speed.Text);
+            car = new Car(car_make,car_year,car_speed);
+            Label_info.Visible = false;
+            Button_done.Visible = false;
+            Panel1.Visible = true;
+        }
+    }
+
+    protected void Button_acc_Click(object sender, EventArgs e)
+    {
+        car_speed=car.acc(car_speed);
+        TextBox_speed.Text = car_speed.ToString();
+    }
+
+    protected void Button_brake_Click(object sender, EventArgs e)
+    {
+        car_speed=car.brake(car_speed);
+        TextBox_speed.Text = car_speed.ToString();
+    }
+
+    protected void Button_restart_Click(object sender, EventArgs e)
+    {
+        car_make = "";
+        car_speed = 0;
+        car_year = 0;
+        TextBox_make.Text = "";
+        TextBox_year.Text = "";
+        TextBox_speed.Text = "";
+        Panel1.Visible = false;
+        Button_done.Visible = true;
+
+    }
+}
